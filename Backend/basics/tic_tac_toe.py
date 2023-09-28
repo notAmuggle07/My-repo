7d1b4f1e266278a8846c68fa0f22f15dfa2c58f7
notAmuggle07 <144878524+notAmuggle07@users.noreply.github.com>
2023-09-28 17:32:21 +0530

    Update tic_tac_toe.py

--- Backend/basics/tic_tac_toe.py
+++ Backend/basics/tic_tac_toe.py
@@ -3,17 +3,14 @@ def create_tic_tac_toe_grid(size):
         print("Grid size must be at least 3.")
         return
 
-    # Define Unicode characters for full-width horizontal and vertical lines
-    horizontal_line = "━━"  # U+2501
-    vertical_line = "┃"  # U+2503
+    horizontal_line = "━━"
+    vertical_line = "┃"
 
     for i in range(size * 2 - 1):
         if i % 2 == 0:
-            # Print the rows with vertical lines and adjusted spacing
             row = vertical_line.join(["  " * 2 + " "] * size)
             print(row)
         else:
-            # Print the rows with horizontal lines and gaps
             row = horizontal_line.join([" " * 2 + " "] * (size +1 ))
             print(row)
 
